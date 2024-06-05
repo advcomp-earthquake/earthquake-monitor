466a26364ab08beb52866fbeb8006276e14b3832
Reinhartos <162394140+Reinhartos@users.noreply.github.com>
2024-06-05 17:40:00 +0800

    add zenoh

---
+++ final/z_subscriber.py
@@ -0,0 +1,8 @@
+import zenoh, time
+def listener(sample):
+    print(f"Received {sample.kind} ('{sample.key_expr}': '{sample.payload.decode('utf-8')}')")
+if __name__ == "__main__":
+    session = zenoh.open()
+    sub = session.declare_subscriber('demo/kitchen/temp', 
+listener)
+time.sleep(60)
