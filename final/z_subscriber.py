9d13af24cc47d8b7ddbffdc23e690c7b8bab25e1
aryawidiap <aryawidiaputra2002@gmail.com>
2024-06-12 01:46:41 +0700

    Delete unnecessary data

--- final/z_subscriber.py
+++
@@ -1,8 +0,0 @@
-import zenoh, time
-def listener(sample):
-    print(f"Received {sample.kind} ('{sample.key_expr}': '{sample.payload.decode('utf-8')}')")
-if __name__ == "__main__":
-    session = zenoh.open()
-    sub = session.declare_subscriber('demo/kitchen/temp', 
-listener)
-time.sleep(60)
